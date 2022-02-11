b55f11b5281c76329e19e6d832140254ed4a7732
Santiago Angulo <santiago.angulo@imgarena.com>
2022-02-11 17:20:02 +0000

    Trying to add some pack of cards and logic to it

--- src/Game.tsx
+++ src/Game.tsx
@@ -15,11 +15,45 @@ export const Game: React.FC<GameProps> = ({ players }) => {
     [players]
   );
 
+  /* I'd like to create a "shuffled" pack of cards. First the pack,
+    then the shuffled one we will be drawing cards in order to each player each round. */
+  let packOfCards: Array<number> = [];
+  for (let iter = 0; iter < 100; iter++) {
+    packOfCards.push(iter + 1);
+  }
+
+  /* Randomize array in-place using Durstenfeld shuffle algorithm */
+  const shuffleArray = (array: Array<number>) => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  /* 
+  For current round R assign the first cards to each of the P players.
+  Then all cards are returned to the pack before progressing to the next round.
+  */
+
   return (
     <>
-      {players.map((x, i) => (
-        <span key={i}>{x}</span>
-      ))}
+      <ul>
+        {players.map((name, index) => (
+          <>
+            <li key={name}>
+              Player {index} is {name}
+            </li>
+          </>
+        ))}
+        <li>This game has {players.length} players</li>
+        <li>This game has {numberOfRounds} rounds</li>
+      </ul>
+      <ul>
+        <li>The ordered pack of cards is: {packOfCards}</li>
+        <li>The shuffled pack of cards is: {shuffleArray(packOfCards)}</li>
+      </ul>
     </>
   );
 };
